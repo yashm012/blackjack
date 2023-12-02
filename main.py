b63bb61e6b70744b9acb41de080194283f6715ca
yashm012 <118619038+yashm012@users.noreply.github.com>
2023-12-01 20:19:55 -0600

    Update main.py
    better playing and options

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import os
 # Set up the deck
 suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
 ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
-values = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
+values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'Jack': 10, 'Queen': 10, 'King': 10,
+          'Ace': 11}
+money: int
+bet: int
 
 deck = []
 for suit in suits:
     for rank in ranks:
-        deck.append( (rank, suit) )
+        deck.append((rank, suit))
 
 random.shuffle(deck)
 
@@ -21,46 +24,40 @@ for i in range(1):
 for i in range(2):
     player_hand.append(deck.pop())
 
-# Player turns
+money = int(input("How much money would you like to deposit? $"))
+bet = int(input("How much would you like to bet? $"))
+
+# Players turn
 while True:
-    
-    print("Dealer Showing:", *dealer_hand, "('?', '???')", sep='\n')
+    print("\nDealer Showing:", *dealer_hand, "('?', '???')", sep='\n')
     print("\nPlayer Hand:", *player_hand, sep='\n')
-    print("Player Hand Total:", sum(values[rank] for rank,suit in player_hand))
-    
+    print("Player Hand Total:", sum(values[rank] for rank, suit in player_hand))
+
     hit_or_stand = input("Hit or Stand? (h/s) ")
-    
     if hit_or_stand == 'h':
         os.system('clear')
         player_hand.append(deck.pop())
-        if sum(values[rank] for rank,suit in player_hand) > 21:
+        if sum(values[rank] for rank, suit in player_hand) > 21:
             print("Player Hand:", *player_hand, sep='\n')
-            print("Player Hand Total:", sum(values[rank] for rank,suit in player_hand))
-            print("\nBust! Dealer wins.")
+            print("Player Hand Total:", sum(values[rank] for rank, suit in player_hand))
+            print("\nBust! Dealer wins.\nMoney Available: $", money - bet)
             break
-
     elif hit_or_stand == 's':
         break
-        
-# Dealer turns
-if sum(values[rank] for rank,suit in player_hand) <= 21:
 
-    while sum(values[rank] for rank,suit in dealer_hand) < 17:
+# Dealers turn
+if sum(values[rank] for rank, suit in player_hand) <= 21:
+
+    while sum(values[rank] for rank, suit in dealer_hand) < 17:
         dealer_hand.append(deck.pop())
-        if sum(values[rank] for rank,suit in dealer_hand) < 17:
-            print("\nDealer Hand:", *dealer_hand, "('?', '???')", sep='\n')
-        else:
-            break
-
     print("\nDealer Hand:", *dealer_hand, sep='\n')
-    print("Dealer Hand Total:", sum(values[rank] for rank,suit in dealer_hand))
+    print("Dealer Hand Total:", sum(values[rank] for rank, suit in dealer_hand))
 
-    if sum(values[rank] for rank,suit in dealer_hand) > 21:
-        print("Dealer Busts! You win.")
-    elif sum(values[rank] for rank,suit in dealer_hand) < sum(values[rank] for rank,suit in player_hand):
-        print("You win!")
-    elif sum(values[rank] for rank,suit in dealer_hand) > sum(values[rank] for rank,suit in player_hand):
-        print("Dealer wins.")
+    if sum(values[rank] for rank, suit in dealer_hand) > 21:
+        print("Dealer Busts! You win.\nMoney Available: $", money + bet)
+    elif sum(values[rank] for rank, suit in dealer_hand) < sum(values[rank] for rank, suit in player_hand):
+        print("You win!\nMoney Available: $", money + bet)
+    elif sum(values[rank] for rank, suit in dealer_hand) > sum(values[rank] for rank, suit in player_hand):
+        print("Dealer wins.\nMoney Available: $", money - bet)
     else:
-        print("It's a tie.")
-    
+        print("It's a tie.\nMoney Available: $", money)
